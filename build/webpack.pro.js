68a2cf60f1e493deedb8d2f97b0f624cd3a60acc
lxy <lxyuid@126.com>
2018-09-22 10:06:56 +0800

    生产环境配置

--- build/webpack.pro.js
+++ build/webpack.pro.js
@@ -1,65 +1,25 @@
-var base = require("./webpack.base.js"),
-	HtmlWebpackPlugin = require("html-webpack-plugin"),
-	BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-		.BundleAnalyzerPlugin;
-(WebpackMd5Hash = require("webpack-md5-hash")),
-	(HtmlWebpackPlugin = require("html-webpack-plugin")),
-	(OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin")),
-	(extractTextWebpackPlugin = require("extract-text-webpack-plugin")),
-	// MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var base = require('./webpack.base.js'),
+  merge = require('webpack-merge'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin,
+  WebpackMd5Hash = require('webpack-md5-hash');
 
-	(module.exports = {
-		mode: "production",
-		entry: base.entry,
-		output: base.output,
-		module: {
-			rules: base.loaders.concat([
-				{
-					test: /\.scss$/,
-					exclude: /node_modules/,
-					use: extractTextWebpackPlugin.extract({
-						fallback: "style-loader",
-						use: [
-							"css-loader",
-							"postcss-loader",
-							"resolve-url-loader",
-							"sass-loader"
-						]
-					})
-				}
-			])
-		},
-		resolve: {
-			extensions: base.extensions,
-			alias: base.alias
-		},
-		externals: base.externals,
-		plugins: [
-			new extractTextWebpackPlugin({
-				filename: "css/[name].css?_v=[chunkhash:8]"
-			}),
-			new OptimizeCssAssetsPlugin({
-				cssProcessor: require("cssnano"),
-				cssProcessorOptions: {
-					safe: true,
-					discardComments: { removeAll: true }
-				},
-				canPrint: true
-			}),
-			new WebpackMd5Hash(),
-			new BundleAnalyzerPlugin(),
-			new HtmlWebpackPlugin({
-				filename: "index.html",
-				template: "index.html",
-				chunks: ["main", "vendor"],
-				inject: true,
-				chunksSortMode: "auto"
-				/* minify: {
+module.exports = merge(base, {
+    mode: 'production',
+    plugins: [
+        new WebpackMd5Hash(),
+        new BundleAnalyzerPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            chunks: ['main', 'vendor'],
+            inject: true,
+            chunksSortMode: 'auto',
+            minify: {
                 removeComments: true,
                 collapseWhitespace: true,
                 removeAttributeQuotes: true
-            } */
-			})
-		],
-		optimization: base.optimization
-	});
+            }
+        })
+    ]
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require("gulp"),
+  gulpSequence = require("gulp-sequence"),
   gutil = require("gulp-util"),
+  del = require("del"),
+  uglify = require("gulp-uglify"),
+  imagemin = require("gulp-imagemin"),
   express = require("express"),
   webpack = require("webpack"),
 	webpackDevMiddleware = require("webpack-dev-middleware"),
   webpackHotMiddleware = require("webpack-hot-middleware"),
   history = require("connect-history-api-fallback"),
-	opn = require("opn");
-
-gulp.task("webpackDevServer", function() {
+  opn = require("opn");
+  
+// 开发服务器
+gulp.task("dev", function() {
   var webpackDevConfig = require("./build/webpack.dev.js");
 
 	webpackDevConfig.entry = ['react-hot-loader/patch', "webpack-hot-middleware/client?noInfo=true"].concat(
@@ -47,3 +52,57 @@ gulp.task("webpackDevServer", function() {
 		opn("http://localhost:3008");
 	});
 });
+
+//清除
+gulp.task("clean", function(cb) {
+	del.sync("dist");
+	cb();
+});
+
+//图片压缩
+gulp.task("copyImg", function() {
+	return gulp
+		.src("img/**/*.*")
+		.pipe(imagemin())
+		.pipe(gulp.dest("dist/img/"));
+});
+
+//复制无需编译的js文件
+gulp.task("copyJs", function() {
+	return gulp
+		.src([
+			"src/js_modules/react/pro/react.js",
+			"src/js_modules/react-dom/pro/react-dom.js"
+		])
+		.pipe(uglify())
+		.pipe(gulp.dest("dist/js/"));
+});
+
+//webpack production
+gulp.task("webpackPro", function(cb) {
+	var webpackProConfig = require("./build/webpack.pro.js");
+	webpack(webpackProConfig, function(err, stats) {
+		if (err) throw new gutil.PluginError("webpack:production", err);
+		gutil.log(
+			"[webpack:production]",
+			stats.toString({
+				chunks: false,
+				colors: true,
+				timings: true,
+				source: true,
+				cachedAssets: false
+			})
+		);
+		cb();
+	});
+});
+
+gulp.task("buildSuccess", function(cb) {
+	gutil.log("[webpack:production]", "build success!");
+	cb();
+});
+
+gulp.task(
+	"build",
+	gulpSequence("clean", "copyImg", "copyJs", "webpackPro", "buildSuccess")
+);
